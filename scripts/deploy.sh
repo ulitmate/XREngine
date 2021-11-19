f9f2f9a98cd4924ea8b99691e1e8e84bc97e8b21
Kyle Baran <kbaran@bitscoop.com>
2021-11-19 15:42:08 -0800

    fixup

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,4 +4,4 @@ set -x
 STAGE=$1
 TAG=$2
 
-helm upgrade --reuse-values --set analytics.image.repository=public.ecr.aws/x0k5y3h8/xrengine-$TAG-analytics,analytics.image.tag=$TAG,api.image.repository=public.ecr.aws/x0k5y3h8/xrengine-$TAG-api,api.image.tag=$TAG,gameserver.image.repository=public.ecr.aws/x0k5y3h8/xrengine-$TAG-gameserver,gameserver.image.tag=$TAG,client.image.repository=public.ecr.aws/x0k5y3h8/xrengine-$TAG-client,client.image.tag=$TAG $STAGE xrengine/xrengine
+helm upgrade --reuse-values --set analytics.image.repository=public.ecr.aws/x0k5y3h8/xrengine-$STAGE-analytics,analytics.image.tag=$TAG,api.image.repository=public.ecr.aws/x0k5y3h8/xrengine-$STAGE-api,api.image.tag=$TAG,gameserver.image.repository=public.ecr.aws/x0k5y3h8/xrengine-$STAGE-gameserver,gameserver.image.tag=$TAG,client.image.repository=public.ecr.aws/x0k5y3h8/xrengine-$STAGE-client,client.image.tag=$TAG $STAGE xrengine/xrengine
