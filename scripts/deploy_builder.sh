80f239f251bcd3ec3506973a044aa559b9e590b2
nouman-iftikhar <nouman72884@gmail.com>
2021-12-20 08:57:27 +0500

    update bash scripts

--- scripts/deploy_builder.sh
+++ scripts/deploy_builder.sh
@@ -4,4 +4,4 @@ set -x
 STAGE=$1
 TAG=$2
 
-helm upgrade --install --reuse-values --set builder.image.tag=$TAG $STAGE-builder xrengine/xrengine-builder
+helm upgrade --install --reuse-values -f ../packages/ops/configs/dev.builder.template.values.yaml --set builder.image.tag=$TAG $STAGE-builder xrengine/xrengine-builder
